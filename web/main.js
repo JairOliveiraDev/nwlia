f3bff5e917cc49c6e503f3fa08a2634d562077a6
Jair Oliveira <jair.correa.oliveira@gmail.com>
2023-09-22 20:36:28 -0300

    add video to audio conversor

--- web/main.js
+++ web/main.js
@@ -4,3 +4,7 @@ import "./form.js"
 import "./styles/base.css"
 import "./styles/app.css"
 import "./styles/form.css"
+
+//dependencias
+//npm i @xenova/transformers fluent-ffmpeg ffmepeg-static node-wav
+
